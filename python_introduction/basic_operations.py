934be73fe9bfa3e7a7550eb9617042077df277e1
Okello Obera <okellitto@gmail.com>
2025-07-16 14:07:23 +0300

    basic operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = 10 + 5
+subtraction = 10 - 5
+multiplication = 10 * 5
+
+print(f"addition of {10} and {5} is {addition})
+print(f"subtraction of {10} and {5} is {subtraction})
+print(f"multiplication of {10} and {5} is {multiplication})
